b57f3a22f7b54a3cc3f987ac255bbb631ee0f67a
MichaelGorman2004 <gorman.270@osu.edu>
2026-02-22 18:37:08 -0500

    feat(routes): register /venues route for venue discovery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react';
 import { QueryProvider } from './providers/QueryProvider';
 import { AppShell } from './layout';
 import { DashboardPage } from './features/dashboard';
+import { VenueBrowse } from './features/venues';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 
@@ -42,6 +43,7 @@ function App() {
               )}
             >
               <Route index element={<DashboardPage />} />
+              <Route path="venues" element={<VenueBrowse />} />
             </Route>
           </Routes>
         </BrowserRouter>
